9755c4e033c0fb8eccd050f9897e3666a40f6c91
negu63 <negu6633@gmail.com>
2022-05-04 14:55:13 +0900

    Remove api welcome

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import receiveSignature from "./routes/receive-signature";
 import acceptSignature from "./routes/accept-signature";
-import welcome from "./routes/welcome";
 
 export default () => {
   const app = Router();
   receiveSignature(app);
   acceptSignature(app);
-  welcome(app);
 
   return app;
 };
