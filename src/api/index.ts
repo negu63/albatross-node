c360901e30f8aad2fce9de9d6595f22e039d97ad
negu63 <negu6633@gmail.com>
2022-05-07 21:05:59 +0900

    Add api route signature/reject

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import receiveSignature from "./routes/receive-signature";
 import acceptSignature from "./routes/accept-signature";
+import rejectSignature from "./routes/reject-signature.js";
+import testSignature from "./routes/test-signature";
 
 export default () => {
   const app = Router();
   receiveSignature(app);
   acceptSignature(app);
+  rejectSignature(app);
+  testSignature(app);
 
   return app;
 };
