84e395455f2dab72ab6544f53de493f1869ec4fe
negu63 <negu6633@gmail.com>
2022-05-10 22:58:36 +0900

    Add api route message-send

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import acceptSignature from "./routes/signature/accept";
 import rejectSignature from "./routes/signature/reject.js";
 import sendSignature from "./routes/signature/send.js";
+import sendMessage from "./routes/message/send.js";
 
 export default () => {
   const app = Router();
+  
   sendSignature(app);
   acceptSignature(app);
   rejectSignature(app);
-  testSignature(app);
 
+  sendMessage(app);
   return app;
 };
