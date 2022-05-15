34ce38ea57b55794e18b83c2b51a2b42d92b8df0
negu63 <negu6633@gmail.com>
2022-05-15 15:27:37 +0900

    Add api route signature-send-relay

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import sendSignature from "./routes/signature/send.js";
+import sendSignatureRelay from "./routes/signature/send-realy";
 import acceptSignature from "./routes/signature/accept";
 import rejectSignature from "./routes/signature/reject.js";
 import listSignature from "./routes/signature/list.js";
@@ -9,6 +10,7 @@ export default () => {
   const app = Router();
 
   sendSignature(app);
+  sendSignatureRelay(app);
   acceptSignature(app);
   rejectSignature(app);
   listSignature(app);
