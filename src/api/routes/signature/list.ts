df3a054d202e4b7c4d36be3b4002110654ddf0dc
negu63 <negu6633@gmail.com>
2022-05-16 00:04:46 +0900

    Change api path signature-list

--- src/api/routes/signature/list.ts
+++ src/api/routes/signature/list.ts
@@ -11,7 +11,7 @@ export default (app: Router) => {
   app.use("/signature", route);
 
   route.get(
-    "/",
+    "/all",
     middlewares.isValidSignature,
     async (req: Request, res: Response) => {
       // Generate signature list
