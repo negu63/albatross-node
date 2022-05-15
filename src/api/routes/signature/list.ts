0041acd575a7c5f7d4bb87600b48b8215ebcee2a
negu63 <negu6633@gmail.com>
2022-05-15 15:26:09 +0900

    Add api signature-list

---
+++ src/api/routes/signature/list.ts
@@ -0,0 +1,46 @@
+import { Request, Response, Router } from "express";
+import fs from "fs";
+import { JSONFile, Low } from "lowdb";
+import { Signature } from "../../../interfaces/Signature.js";
+import { encryptWithECDH } from "../../../lib/ecdh.js";
+import middlewares from "../../middlewares/index.js";
+
+const route = Router();
+
+export default (app: Router) => {
+  app.use("/signature", route);
+
+  route.get(
+    "/",
+    middlewares.isValidSignature,
+    async (req: Request, res: Response) => {
+      // Generate signature list
+      const signatureDir = `./signatures`;
+      if (fs.existsSync(signatureDir)) {
+        const signatures: string[] = [];
+        const signatureFiles = fs.readdirSync(signatureDir);
+        await Promise.all(
+          signatureFiles.map(async (signatureFile) => {
+            const signaturePath = `${signatureDir}/${signatureFile}`;
+            // Read the signature
+            const adapter = new JSONFile<Signature>(signaturePath);
+            const db = new Low(adapter);
+            await db.read();
+            signatures.push(
+              await encryptWithECDH(
+                JSON.stringify(db.data),
+                process.env.PRIVATE_KEY,
+                process.env.PUBLIC_KEY
+              )
+            );
+          })
+        );
+        res
+          .status(200)
+          .json({ message: "Fetch complete.", encryptedData: signatures });
+      } else {
+        res.status(500).json({ message: "Failed." });
+      }
+    }
+  );
+};
