2c5c85994aaa7e1464d5539c9f84a6ddb08ae1a5
negu63 <negu6633@gmail.com>
2022-05-22 17:17:31 +0900

    Remove old api routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,8 @@
 import { Router } from "express";
-import sendSignature from "./routes/signature/send.js";
-import sendSignatureRelay from "./routes/signature/send-relay";
-import acceptSignature from "./routes/signature/accept";
-import rejectSignature from "./routes/signature/reject.js";
-import getSignature from "./routes/signature/get.js";
-import listSignature from "./routes/signature/list.js";
-import sendMessage from "./routes/message/send.js";
 
 export default () => {
   const app = Router();
 
-  sendSignature(app);
-  sendSignatureRelay(app);
-  acceptSignature(app);
-  rejectSignature(app);
-  getSignature(app)
-  listSignature(app);
 
-  sendMessage(app);
   return app;
 };
