f1bf50b27a7a42427bebfb25c9c9fed28cfe771d
negu63 <negu6633@gmail.com>
2022-05-22 19:49:24 +0900

    Modify api signature-get

--- src/api/routes/signatures/get.ts
+++ src/api/routes/signatures/get.ts
@@ -1,20 +1,21 @@
 import { Request, Response, Router } from "express";
-import middlewares from "../../middlewares/index.js";
 import fs from "fs";
-import { decryptWithECDH } from "../../../lib/ecdh.js";
+import { decryptWithECDH, encryptWithECDH } from "../../../lib/ecdh.js";
+import { JSONFile, Low } from "lowdb";
+import { Signature } from "../../../interfaces/Signature.js";
+import { NodeInfo } from "../../../interfaces/NodeInfo.js";
+import axios from "axios";
 
 const route = Router();
 
 export default (app: Router) => {
-  app.use("/signature", route);
+  app.use("/signatures", route);
 
-  route.get(
-    "/",
-    middlewares.isValidSignature,
-    async (req: Request, res: Response) => {
+  route.get("/", async (req: Request, res: Response) => {
+    try {
       // Decrypt Data
       const publicB = req.body.publicKey;
-      const data = JSON.parse(
+      const reqData = JSON.parse(
         await decryptWithECDH(
           req.body.encryptedData,
           process.env.PRIVATE_KEY!,
@@ -23,13 +24,59 @@ export default (app: Router) => {
       );
 
       // Find signature
-      const sigPath = `sinatures/${data.publicKey}.json`;
+      const sigPath = `signatures/${reqData.queryPublicKey}.json`;
       const isExist = fs.existsSync(sigPath);
+
       if (isExist) {
-        res.status(200).json({});
+        const adapter = new JSONFile<Signature>(sigPath);
+        const db = new Low(adapter);
+        await db.read();
+
+        const encryptedData = await encryptWithECDH(
+          JSON.stringify(db.data),
+          process.env.PRIVATE_KEY,
+          reqData.publicKey
+        );
+
+        res
+          .status(200)
+          .json({ publicKey: process.env.PUBLIC_KEY, encryptedData });
       } else {
-        res.status(500);
+        // Query signature
+        res.status(303).send();
+        const data = {
+          ip: process.env.PUBLIC_IP,
+          publicKey: process.env.PUBLIC_KEY,
+          queryPublicKey: reqData.queryPublicKey,
+        };
+
+        const adapter = new JSONFile<NodeInfo[]>("node-info.json");
+        const db = new Low(adapter);
+        await db.read();
+
+        db.data.forEach(async (nodeInfo) => {
+          const encryptedData = await encryptWithECDH(
+            JSON.stringify(data),
+            process.env.PRIVATE_KEY,
+            nodeInfo.publicKey
+          );
+
+          await axios.post(
+            `http://${nodeInfo.ip}:3000/api/signatures/query`,
+            JSON.stringify({
+              publicKey: process.env.PUBLIC_KEY,
+              encryptedData,
+            }),
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        });
       }
+    } catch (error) {
+      res.status(500).send();
     }
-  );
+  });
 };
