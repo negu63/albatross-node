52ea5030f93904ea5715f84146ba094290bacff7
negu63 <negu6633@gmail.com>
2022-05-22 17:17:40 +0900

    Add api signature-sign

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import signSignature from "./routes/signatures/sign";
 
 export default () => {
   const app = Router();
 
+  signSignature(app);
 
   return app;
 };
