23b5a1753f1d1d65d9a598da03b61ff54bc3985b
negu63 <negu6633@gmail.com>
2022-05-22 19:52:14 +0900

    Add api route signature-get, query, share

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import signSignature from "./routes/signatures/sign";
+import getSignature from "./routes/signatures/get";
+import querySignature from "./routes/signatures/query";
+import shareSignature from "./routes/signatures/share";
 
 export default () => {
   const app = Router();
 
   signSignature(app);
+  getSignature(app);
+  querySignature(app)
+  shareSignature(app)
 
   return app;
 };
