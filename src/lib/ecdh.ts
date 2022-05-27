141eb103edca0b17541973171fee302309434e06
negu63 <negu6633@gmail.com>
2022-05-28 00:34:26 +0900

    Modify ECDH en/decrypt function

--- src/lib/ecdh.ts
+++ src/lib/ecdh.ts
@@ -18,10 +18,17 @@ async function encryptWithECDH(
   privateA: string,
   publicB: string
 ) {
-  return CryptoJS.AES.encrypt(
-    data,
-    await hexECDHKey(privateA, publicB)
-  ).toString();
+  const iv = secp.utils.bytesToHex(secp.utils.randomBytes(16));
+  return (
+    iv +
+    CryptoJS.AES.encrypt(
+      data,
+      CryptoJS.enc.Hex.parse(await hexECDHKey(privateA, publicB)),
+      {
+        iv: CryptoJS.enc.Hex.parse(iv),
+      }
+    ).toString()
+  );
 }
 
 async function decryptWithECDH(
@@ -30,8 +37,11 @@ async function decryptWithECDH(
   publicB: string
 ) {
   return CryptoJS.AES.decrypt(
-    encryptedData,
-    await hexECDHKey(privateA, publicB)
+    encryptedData.slice(32),
+    CryptoJS.enc.Hex.parse(await hexECDHKey(privateA, publicB)),
+    {
+      iv: CryptoJS.enc.Hex.parse(encryptedData.slice(0, 32)),
+    }
   ).toString(CryptoJS.enc.Utf8);
 }
 
